97b07f04de89f2716744ae67071d4b297d09c861
xmontr <xmontr@live.com>
2017-07-04 15:00:32 +0200

    update startserver  wildfly

--- wildfly/docker/startServer.bash
+++ wildfly/docker/startServer.bash
@@ -10,8 +10,8 @@ fi
 
 
 
-#exec 1> $FILE
-#exec 2> $FILE 
+exec 1> $FILE
+exec 2> $FILE 
 
 echo " starting wildfly in standalone mode "
 /opt/jboss/wildfly/bin/standalone.sh  -b  0.0.0.0 -bmanagement 0.0.0.0   
